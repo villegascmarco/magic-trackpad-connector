0fc18eb73a084a05c0263fe1d7f3dce6d2e4513a
Isai Marco Polo Villegas Cervantes <villegascmarco@gmail.com>
2026-05-17 13:19:39 -0600

    Initial Commit

---
+++ magic-trackpad-connector/magic_trackpad_connectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic_trackpad_connectorApp.swift
+//  magic-trackpad-connector
+//
+//  Created by Isai Villegas on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct magic_trackpad_connectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
